b488056140635d8f69a6107445adbc0c0a5292dd
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-01-18 00:48:25 +0300

    Add project files.

---
+++ online commercial automation/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace online_commercial_automation
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ online commercial automation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="online_commercial_automation.MvcApplication" Language="C#" %>
