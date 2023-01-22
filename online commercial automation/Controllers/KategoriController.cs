0ea726b37db40fd950bc9c88fd748bbbe361db60
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-01-22 18:52:47 +0300

    Urunler Bitti Guncelelemede Hatalar Var Sonradan donulecek

--- online commercial automation/Controllers/KategoriController.cs
+++ online commercial automation/Controllers/KategoriController.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using online_commercial_automation.Models.Siniflar;
 
 namespace online_commercial_automation.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
         {
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
+        }
+
+        //Kategori Ekleme Kismi
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Kategori Silme Kismi Kotu yol
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Kategori Getirme(Guncelleme icin)  yol
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+        //Kategori Guncelleme  yol
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAD = k.KategoriAD;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
