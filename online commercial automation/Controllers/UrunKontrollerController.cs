e438ed027920b59c4b289430cd68d9eaf5977f03
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-01-22 23:41:17 +0300

    dropdownlist yapma

--- online commercial automation/Controllers/UrunKontrollerController.cs
+++ online commercial automation/Controllers/UrunKontrollerController.cs
@@ -13,18 +13,34 @@ namespace online_commercial_automation.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Uruns.ToList();
+            var urunler = c.Uruns.Where(x => x.Durum == true).ToList();
             return View(urunler);
         }
+        //yeni urun ekleme islemi
         [HttpGet]
         public ActionResult YeniUrun()
-        {
+        { //yeni urun ekleme dropdownlistfor Important
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                       select new SelectListItem
+                                       {
+                                           Text =x.KategoriAD,
+                                           Value=x.KategoriID.ToString()
+                                       }).ToList();
+            ViewBag.dgr1 = deger1;
             return View();
         }
         [HttpPost]
         public ActionResult YeniUrun(Urun p)
         {
             c.Uruns.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //urun silme islemi  Durumu False Yapma Iyi Yol Bu 
+        public ActionResult UrunSil(int id)
+        {
+            var deger = c.Uruns.Find(id);
+            deger.Durum = false;
             c.SaveChanges();
             return RedirectToAction("Index");
         }
