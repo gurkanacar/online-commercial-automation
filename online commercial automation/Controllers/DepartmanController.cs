171f42c7b82b8c7e146c117b2a3629fa43c13ded
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-01-23 23:59:22 +0300

    departman baslangic

---
+++ online commercial automation/Controllers/DepartmanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using online_commercial_automation.Models.Siniflar;
+
+namespace online_commercial_automation.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmen.ToList();
+            return View(degerler);
+        }
+
+    }
+}
