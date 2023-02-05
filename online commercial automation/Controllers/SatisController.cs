a7c64df6fcb72a3da13b01283421310a60678391
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-02-06 02:34:33 +0300

    continue (Sales)

--- online commercial automation/Controllers/SatisController.cs
+++ online commercial automation/Controllers/SatisController.cs
@@ -19,11 +19,34 @@ namespace online_commercial_automation.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAD,
+                                               Value = x.UrunID.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " "+x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger3 = (from x in c.personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAD + " "+x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+
             return View();
         }
         [HttpPost]
         public ActionResult YeniSatis(SatisHareket s)
         {
+            s.Tarih =DateTime.Parse(DateTime.Now.ToShortDateString());
             c.SatisHarekets.Add(s);
             c.SaveChanges();
             return RedirectToAction("Index");
