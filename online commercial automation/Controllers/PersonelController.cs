a7617a390aa2c7b3a96ab3aba2975804becfeb21
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-02-05 12:17:03 +0300

    Personels is finished

---
+++ online commercial automation/Controllers/PersonelController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using online_commercial_automation.Models.Siniflar;
+
+namespace online_commercial_automation.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.personels.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmen.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(personel p)
+        {
+            c.personels.Add(p);
+            c.SaveChanges();  
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmen.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            var prs = c.personels.Find(id);
+            return View("PersonelGetir",prs);
+        }
+        public ActionResult PersonelGuncelle(personel p)
+        {
+            var prsn = c.personels.Find(p.PersonelID);
+            prsn.PersonelAD = p.PersonelAD;
+            prsn.PersonelSoyad = p.PersonelSoyad;
+            prsn.PersonelGorsel = p.PersonelGorsel;
+            prsn.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
