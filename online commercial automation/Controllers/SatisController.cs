aace6d0cd0f4586b190f4778d1187ca798b9c9b7
gurkanacar <61539367+gurkanacar@users.noreply.github.com>
2023-02-06 01:33:10 +0300

    sales is begun

---
+++ online commercial automation/Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using online_commercial_automation.Models.Siniflar;
+
+namespace online_commercial_automation.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
